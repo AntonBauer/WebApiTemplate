2cf36020e4ac9ec8ca8cd46d8447c87fa91717b6
BauerAV <abton.bauer@gmail.com>
2021-03-27 20:58:19 +0100

    Add Swagger

--- WebApiTemplate.WebApi/Startup.cs
+++ WebApiTemplate.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using WebApiTemplate.Application.Extensions;
 
 namespace WebApiTemplate.WebApi
@@ -11,6 +12,10 @@ namespace WebApiTemplate.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddUseCases()
+                    .AddSwaggerGen(options => 
+                    {
+                        options.SwaggerDoc("v01", new OpenApiInfo { Title = "API Doc", Version = "v0.1" });
+                    })
                     .AddMediatR(typeof(Startup))
                     .AddControllers();
         }
@@ -18,6 +23,15 @@ namespace WebApiTemplate.WebApi
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting()
+               .UseSwagger(options =>
+               {
+                   options.RouteTemplate = "docs/{documentName}/swagger.json";
+               })
+               .UseSwaggerUI(options =>
+               {
+                   options.RoutePrefix = "docs";
+                   options.SwaggerEndpoint("v01/swagger.json", "API V1");
+               })
                .UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
